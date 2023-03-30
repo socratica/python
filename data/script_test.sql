906b9597b92bf0b0a796b66fd2dd55b81fe24ccb
Michael Harrison <mlh496@gmail.com>
2023-03-29 18:12:47 -0700

    SQL file used in SQLite video.

---
+++ data/script_test.sql
@@ -0,0 +1,8 @@
+BEGIN;
+CREATE TABLE people (id, fn, ln);
+INSERT INTO people VALUES (1, "Steve", "Coplan");
+INSERT INTO people VALUES (2, "Shawn", "Verzilli");
+INSERT INTO people VALUES (3, "Ben", "Shew");
+INSERT INTO people VALUES (4, "Robert", "Culling");
+INSERT INTO people VALUES (5, "Ryan", "Bennett");
+COMMIT;
